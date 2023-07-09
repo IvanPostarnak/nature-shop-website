f0d7960fdc7ac28f4540cc5f3314ba1d33d2e712
Ivan Postarnak <ivan.postarnak.s@gmail.com>
2023-07-09 07:09:01 +0300

    refactor(posts): extract <Pagination/> from <PostList/> upstairs in <Posts/> page
    Before this commit, all logic of <Posts/> page was actually incapsulated
    in <PostList/>. I've done huge refactoring:
    	1. All posts' data fetching right now is in <Posts/> page (useFetch)
    	2. <PostList/> accepts all data[] as a 'data=' props and render it inside
    	3. <Pagination/> had been promoted - it is rendered by <Posts/> page
    		directly, because all data right now is in root page
    
    So, from now on <Posts/> page is ready to be extended by adding filtering
    options. All tests are passing - everything works exactly the same.
    
    Add +1 mistake in MISTAKES.md:
    + REACT: 'do not evrly incapsulate executing logic, respect Single Responsibility'
    	(read this mistake to find out about this situation)

--- src/pages/Posts/Posts.page.jsx
+++ src/pages/Posts/Posts.page.jsx
@@ -1,4 +1,4 @@
-import { memo } from "react";
+import { memo, useCallback } from "react";
 import AdaptiveRender from "components/layout/AdaptiveRender/AdaptiveRender";
 import Content from "widgets/Content/Content.widget";
 import Main from "components/UI/Main/Main";
@@ -6,10 +6,29 @@ import Sidebar from "widgets/Sidebar/Sidebar.widget";
 import Header from "components/UI/Header/Header";
 import PostList from "widgets/PostList/PostList.widget";
 import H2 from "components/UI/H2/H2";
+import Pagination from "components/Pagination/Pagination";
+import { useSelector } from "react-redux";
+import { getDevice, getPostsAmount, getPostsLastActivePage } from "store/selectors";
+import { usePagination } from "hooks/hooks";
+import { useDispatch } from "react-redux";
+import PostsService from "services/PostsService/PostsService";
+import { setPostsLastActivePage } from "store/actions";
+import Loader from "components/UI/Loader/Loader";
 
 import styles from './Posts.module.scss';
 
 const Posts = () => {
+  const device = useSelector(getDevice);
+  const lastActivePage = useSelector(getPostsLastActivePage);
+  const postsAmount = useSelector(getPostsAmount);
+  const {step, setStart, isLoading, data, headers} = usePagination(PostsService.getOnePage.bind(PostsService));
+  const dispatch = useDispatch();
+
+  const handleChange = useCallback((pageId, newStart) => {
+    dispatch(setPostsLastActivePage(pageId));
+    setStart(newStart);
+  }, []);
+
   return (
     <div
       className={styles.posts}
@@ -20,8 +39,27 @@ const Posts = () => {
           <Main>
             <Header>
               <H2>{`Posts`}</H2>
+              {
+                !headers
+                ? <Loader/>
+                : `Presented ${headers['x-current-amount']} out of ${postsAmount.value}`
+              }
             </Header>
-            <PostList/>
+            {
+              isLoading || !data
+              ? <Loader/>
+              : <PostList
+                  data={data}
+                  device={device}
+                />
+            }
+            <Pagination
+              device={device}
+              totalAmount={postsAmount.value}
+              step={step}
+              active={lastActivePage}
+              onChangePage={handleChange}
+            />
           </Main>
         </Content>
         <Sidebar/>
--- src/widgets/PostList/PostList.widget.jsx
+++ src/widgets/PostList/PostList.widget.jsx
@@ -1,44 +1,21 @@
-import { memo, useCallback } from "react";
-import PostsService from "services/PostsService/PostsService";
-import { useSelector } from "react-redux";
-import { getDevice, getPostsAmount, getPostsLastActivePage } from "store/selectors";
-import { setPostsLastActivePage } from "store/actions";
-import Loader from "components/UI/Loader/Loader";
+import { memo } from "react";
 import Section from "components/UI/Section/Section";
 import PostCard from "components/PostCard/PostCard";
-import Pagination from "components/Pagination/Pagination";
-import { usePagination } from "hooks/hooks";
-import { useDispatch } from "react-redux";
+import PropTypes from 'prop-types';
 
 import styles from './PostList.module.scss';
 
-const PostList = () => {
-  const device = useSelector(getDevice);
-  const postsAmount = useSelector(getPostsAmount);
-  const lastActivePage = useSelector(getPostsLastActivePage);
-  const {step, setStart, isLoading, data, headers} = usePagination(PostsService.getOnePage.bind(PostsService));
-  const dispatch = useDispatch();
-
-  const handleChange = useCallback((pageId, newStart) => {
-    dispatch(setPostsLastActivePage(pageId));
-    setStart(newStart);
-  }, []);
-
+const PostList = ({data, device, ...rest}) => {
   return (
     <div
       className={styles.post_list}
       data-testid="post-list"
+      {...rest}
     >
-      {
-        !headers
-        ? <Loader/>
-        : `Presented ${headers['x-current-amount']} out of ${postsAmount.value}`
-      }
       <Section>
         {
-          isLoading || !data
-          ? <Loader/>
-          : data.map((post) => {
+          data
+          && data.map((post) => {
               return (
                 <PostCard
                   data={post}
@@ -49,19 +26,19 @@ const PostList = () => {
             })
         }
       </Section>
-      {
-        !headers
-        ? <Loader/>
-        : <Pagination
-            device={device}
-            totalAmount={postsAmount.value}
-            step={step}
-            active={lastActivePage}
-            onChangePage={handleChange}
-          />
-      }
     </div>
   )
+};
+
+PostList.propTypes = {
+  data: PropTypes.arrayOf(
+    PropTypes.object.isRequired
+  ).isRequired,
+  device: PropTypes.exact({
+    isMobile: PropTypes.bool.isRequired,
+    isTablet: PropTypes.bool.isRequired,
+    isDesktop: PropTypes.bool.isRequired
+  }).isRequired
 };
 
 export default memo(PostList);
